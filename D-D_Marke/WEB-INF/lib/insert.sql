c3ec1fc8c1b82af14b19ea390459019a062523df
bdg94 <bdg94@WIN-I5Q3EBCU5T8>
2023-11-10 12:20:49 +0900

    this is commit

---
+++ D-D_Marke/WEB-INF/lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO product VALUES('P1234', 'iPhone 6s', 800000, '1334X750 Renina HD display, 8-megapixel iSight Camera','Smart Phone', 'Apple', 1000, 'new', 'P1234.png');
+INSERT INTO product VALUES('P1235', 'LG PC gram', 1500000, '3.3-inch,IPS LED display, 5rd Generation Intel Core processors', 'Notebook', 'LG', 1000, 'new', 'P1235.png');
+INSERT INTO product VALUES('P1236', 'Galaxy Tab S', 900000, '3.3-inch, 212.8*125.6*6.6mm,  Super AMOLED display, Octa-Core processor', 'Tablet', 'Samsung', 1000, 'new', 'P1236.png');
+INSERT INTO product VALUES('P1236', 'Galaxy z flip5', 990000, 'z플립5 (256 GB) · 크기 (Main Display). 170.3 mm · 해상도 (Main). 2640 x 1080 (FHD+) · 종류 (Main). Dynamic AMOLED 2X · 색심도 (Main). 16 M · 크기 (Sub).', 'phone', 'Samsung', 11, 'new', 'zflip52.jpg');
+INSERT INTO product VALUES('P1235', 'LG PC gram', 1500000, '3.3-inch,IPS LED display, 5rd Generation Intel Core processors', 'Notebook', 'LG', 1000, 'new', 'P1235.png');
+INSERT INTO product VALUES('P1234', 'iPhone 15', 990000, '기본 용량 128GB 후면 카메라 4800만 화소, 1억 2천만 화소 울트라와이드 전면 카메라 1200만 화소 배터리 사이즈 3,877 mAh', 'Smart Phone', 'Apple', 1, 'new', 'iphone151.jpg');
+INSERT INTO product VALUES('P0119', 'mac book', 15000000, '칩. Apple M2 Pro 칩 디스플레이. Liquid Retina XDR 디스플레이 메모리. 16GB', '스마트기기', '미국', 1, 'new', 'macbook1.jpg');
+
+select * from product;
